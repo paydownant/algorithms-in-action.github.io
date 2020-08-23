3f5842a7a4c13d8b095ac3437ef2c947e9b8eff6
Yinsong Chen <yinsong.chen@student.unimelb.edu.au>
2020-08-24 03:58:49 +1000

    rewrite linenum highlight to realize collapse

--- src/components/LineNumHighLight.js
+++ src/components/LineNumHighLight.js
@@ -4,29 +4,50 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 // eslint-disable-next-line import/named
 import '../styles/LineNumHighLight.css';
+import findRef from '../pseudocode/findRef';
 
-let lineOfCode = {};
+let codeBlocks = {};
 
 
 function addIndentation(json, name) {
+  const codeBlock = {};
   let pseaudo = '';
   json[name].forEach((line) => {
     if (line['ref'].length > 0) {
       pseaudo = '\xa0\xa0\xa0\xa0'.repeat(line['indentation']) + line['code'];
-      lineOfCode[pseaudo] = line['bookmark'];
+      codeBlock[pseaudo] = line['bookmark'];
       addIndentation(json, line['ref'], line['indentation']);
     } else {
       pseaudo = '\xa0\xa0\xa0\xa0'.repeat(line['indentation']) + line['code'];
-      lineOfCode[pseaudo] = line['bookmark'];
+      codeBlock[pseaudo] = line['bookmark'];
     }
   });
+  codeBlocks[name] = codeBlock;
 }
 
-export const Global = {
-  PAINT_CODELINE: (lineOfCode1, currentBookmark) => {
-    const codeLines = [];
-    let i = 0;
-    for (const [key, value] of Object.entries(lineOfCode1)) {
+
+let i = 0;
+
+function addCollapse(codeBlocks1, currentBookmark, blockName) {
+  let codeLines = [];
+  for (const [key, value] of Object.entries(codeBlocks1[blockName])) {
+    if (findRef(value).length > 0) {
+      codeLines.push(
+        <p
+          key={i}
+          // eslint-disable-next-line react/destructuring-assignment
+          className={currentBookmark.step === value ? 'active' : ''}
+          index={i}
+          role="presentation"
+        >
+          <span>{i + 1}</span>
+          <span>{key}</span>
+        </p>,
+      );
+      i += 1;
+      const temp = addCollapse(codeBlocks1, currentBookmark, findRef(value));
+      codeLines = codeLines.concat(temp);
+    } else {
       codeLines.push(
         <p
           key={i}
@@ -41,13 +62,14 @@ export const Global = {
       );
       i += 1;
     }
-    return codeLines;
-  },
+  }
+  return codeLines;
 };
 
 const LineNumHighLight = () => {
   const { algorithm } = useContext(GlobalContext);
-  lineOfCode = {}
+  codeBlocks = {};
+  i = 0;
   addIndentation(algorithm.pseudocode, 'Main');
 
   /* render data */
@@ -55,7 +77,7 @@ const LineNumHighLight = () => {
   return (
     <div className="line-light">
       <div className="code-container">
-        {Global.PAINT_CODELINE(lineOfCode, algorithm.bookmark)}
+        {addCollapse(codeBlocks, algorithm.bookmark, 'Main')}
       </div>
     </div>
   );

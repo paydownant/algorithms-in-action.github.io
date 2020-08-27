c92a80838f50783301136a41bf2540bb99e60767
nir <>
2020-08-27 17:19:06 +1000

    Reorganised files in the project

--- src/algorithms/binaryTreeSearch.js
+++ src/algorithms/binaryTreeSearch.js
@@ -2,7 +2,7 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable no-multi-spaces,indent,prefer-destructuring */
-import parse from '../pseudocode/parse';
+import parse from '../utils/pseudocode/parse';
 import { BSTExp } from './explanations';
 
 export default {
